36116e6883e65bec0252d76c93e5b82876cc4b99
frankamale <frankamale5@gmail.com>
2023-05-04 13:30:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/smarthomeapp/createRoutine.kt
@@ -0,0 +1,11 @@
+package com.example.smarthomeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class createRoutine : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_routine)
+    }
+}
